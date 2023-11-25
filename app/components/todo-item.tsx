cb76781b7e38184ad43953e5a4e0d0219b15a068
Walter Anderson <walter.anderson@harborhealthteam.com>
2023-11-25 13:24:11 -0600

    updating ui when API call happens

--- app/components/todo-item.tsx
+++ app/components/todo-item.tsx
@@ -3,7 +3,7 @@
 import type { Todo } from "../db";
 import { FaRegSquareCheck, FaRegSquare, FaTrash } from "react-icons/fa6";
 import IconButton from "./icon-button";
-import { useCallback } from "react";
+import { useCallback, useState } from "react";
 
 type TodoItemProps = {
   todo: Todo;
@@ -11,27 +11,35 @@ type TodoItemProps = {
 
 export default function TodoItem(props: TodoItemProps) {
   const { todo } = props;
+  const [completed, setCompleted] = useState(todo.completed);
+  const [deleted, setDeleted] = useState(false);
 
 
   const onToggleComplete = useCallback(async () => {
     await fetch(`/api/todos/${todo.id}`, {
       method: 'PATCH',
-      body: JSON.stringify({ completed: !todo.completed })
+      body: JSON.stringify({ completed: !completed })
     });
-  }, [todo]);
+    setCompleted(!completed);
+  }, [todo, completed]);
 
   const onDelete = useCallback(async () => {
     await fetch(`/api/todos/${todo.id}`, {
       method: 'DELETE',
     });
+    setDeleted(true);
   }, [todo]);
+
+  if (deleted) {
+    return null;
+  }
 
   return (
     <div className="flex flex-row items-center gap-1 w-full">
       <IconButton onClick={onToggleComplete} className="hover:bg-gray-200">
-        {todo.completed ? <FaRegSquareCheck /> : <FaRegSquare />}
+        {completed ? <FaRegSquareCheck /> : <FaRegSquare />}
       </IconButton>
-      <span className={`flex-1 ${todo.completed ? 'line-through' : ''}`}>
+      <span className={`flex-1 ${completed ? 'line-through' : ''}`}>
         {todo.text}
       </span>
       <IconButton onClick={onDelete} className="text-red-600 hover:bg-red-200">
